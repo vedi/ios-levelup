944a3a0fb6122ec3c24f594ec56f34b1984b62b1
Gur Dotan <gurdotan@gmail.com>
2014-05-25 18:03:31 +0300

    Add RangeScore implementation

--- SoomlaiOSBlueprint/SoomlaiOSBlueprint/scoring/RangeScore.h
+++ SoomlaiOSBlueprint/SoomlaiOSBlueprint/scoring/RangeScore.h
@@ -1,13 +1,67 @@
-//
-//  RangeScore.h
-//  SoomlaiOSBlueprint
-//
-//  Created by Gur Dotan on 5/25/14.
-//  Copyright (c) 2014 Soomla. All rights reserved.
-//
+/*
+ Copyright (C) 2012-2014 Soomla Inc.
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 
 #import "Score.h"
 
-@interface RangeScore : Score
+
+@interface Range : NSObject {
+    
+    @private
+    double low;
+    double high;
+}
+
+@property double low;
+@property double high;
+
+- (id)initWithLow:(double)oLow andHigh:(double)oHigh;
+
+- (id)initWithDictionary:(NSDictionary *)dict;
+
+- (NSDictionary*)toDictionary;
 
 @end
+
+
+
+
+
+@interface RangeScore : Score {
+    
+    @private
+    Range* range;
+}
+
+
+@property (strong, nonatomic) Range* range;
+
+
+- (id)initWithScoreId:(NSString *)oScoreId andName:(NSString *)oName andRange:(Range *)oRange;
+
+- (id)initWithScoreId:(NSString *)oScoreId andName:(NSString *)oName andHigherBetter:(BOOL)oHigherBetter andRange:(Range *)oRange;
+
+- (id)initWithDictionary:(NSDictionary *)dict;
+
+- (NSDictionary*)toDictionary;
+
+- (void)incBy:(double)amount;
+
+- (void)decBy:(double)amount;
+
+
+@end
+
+
--- SoomlaiOSBlueprint/SoomlaiOSBlueprint/scoring/RangeScore.m
+++ SoomlaiOSBlueprint/SoomlaiOSBlueprint/scoring/RangeScore.m
@@ -1,13 +1,157 @@
-//
-//  RangeScore.m
-//  SoomlaiOSBlueprint
-//
-//  Created by Gur Dotan on 5/25/14.
-//  Copyright (c) 2014 Soomla. All rights reserved.
-//
+/*
+ Copyright (C) 2012-2014 Soomla Inc.
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 
 #import "RangeScore.h"
+#import "BPJSONConsts.h"
+
+
+@implementation Range
+
+@synthesize low, high;
+
+- (id)initWithLow:(double)oLow andHigh:(double)oHigh {
+    self = [super init];
+    if ([self class] == [Score class]) {
+        @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                       reason:@"Error, attempting to instantiate AbstractClass directly." userInfo:nil];
+    }
+    
+    if (self) {
+        self.low = oLow;
+        self.high = oHigh;
+    }
+    return self;
+}
+
+- (id)initWithDictionary:(NSDictionary *)dict {
+    self = [super init];
+    if ([self class] == [Score class]) {
+        @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                       reason:@"Error, attempting to instantiate AbstractClass directly." userInfo:nil];
+    }
+    
+    if (self) {
+        self.low = [[dict objectForKey:BP_SCORE_RANGE_LOW] doubleValue];
+        self.high = [[dict objectForKey:BP_SCORE_RANGE_HIGH] doubleValue];
+    }
+    
+    return self;
+}
+
+- (NSDictionary*)toDictionary {
+    return [[NSDictionary alloc] initWithObjectsAndKeys:
+            [NSNumber numberWithDouble:self.low], BP_SCORE_RANGE_LOW,
+            [NSNumber numberWithDouble:self.high], BP_SCORE_RANGE_HIGH,
+            nil];
+}
+
+
+@end
+
+
+
+
 
 @implementation RangeScore
+
+@synthesize range;
+
+
+- (id)initWithScoreId:(NSString *)oScoreId andName:(NSString *)oName andRange:(Range *)oRange {
+    self = [super init];
+    if ([self class] == [Score class]) {
+        @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                       reason:@"Error, attempting to instantiate AbstractClass directly." userInfo:nil];
+    }
+    
+    if (self) {
+        self.scoreId = oScoreId;
+        self.name = oName;
+        self.range = oRange;
+    }
+    return self;
+}
+
+- (id)initWithScoreId:(NSString *)oScoreId andName:(NSString *)oName andHigherBetter:(BOOL)oHigherBetter andRange:(Range *)oRange {
+    self = [super init];
+    if ([self class] == [Score class]) {
+        @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                       reason:@"Error, attempting to instantiate AbstractClass directly." userInfo:nil];
+    }
+    
+    if (self) {
+        self.scoreId = oScoreId;
+        self.name = oName;
+        self.higherBetter = oHigherBetter;
+        self.range = oRange;
+    }
+    return self;
+}
+
+- (id)initWithDictionary:(NSDictionary *)dict {
+    self = [super init];
+    if ([self class] == [Score class]) {
+        @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                       reason:@"Error, attempting to instantiate AbstractClass directly." userInfo:nil];
+    }
+    
+    if (self) {
+        self.scoreId = [dict objectForKey:BP_SCORE_SCOREID];
+        self.name = [dict objectForKey:BP_NAME];
+        self.higherBetter = [[dict objectForKey:BP_SCORE_HIGHBETTER] boolValue];
+        self.range = [[Range alloc] initWithDictionary:[dict objectForKey:BP_SCORE_RANGE]];
+    }
+    return self;
+}
+    
+- (NSDictionary*)toDictionary {
+    return [[NSDictionary alloc] initWithObjectsAndKeys:
+            self.scoreId, BP_SCORE_SCOREID,
+            self.name, BP_NAME,
+            self.higherBetter, BP_SCORE_HIGHBETTER,
+            [self.range toDictionary], BP_SCORE_RANGE,
+            nil];
+}
+    
+- (void)incBy:(double)amount {
+    
+    // Don't increment if we've hit the range's highest value
+    if ([self tempScore] == self.range.high) return;
+    [super incBy:amount];
+}
+
+- (void)decBy:(double)amount {
+
+    // Don't increment if we've hit the range's lowest value
+    if ([self tempScore] == self.range.low) return;
+    [super decBy:amount];
+}
+
+// TODO: document setter override
+// TODO: Consult Refael
+- (void)setTempScore:(double)scoreValue {
+    if (scoreValue > self.range.high) {
+        scoreValue = self.range.high;
+    }
+    if (scoreValue < self.range.low) {
+        scoreValue = self.range.low;
+    }
+    super.tempScore = scoreValue;
+}
+
+
 
 @end
