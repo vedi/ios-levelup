59bf545fa16a20a23765c3168a882d8b6eead7f6
Lassic <homedev@gmail.com>
2014-07-05 16:14:21 +0300

    new binaries built

--- build/headers/public/LevelUp.h
+++ build/headers/public/LevelUp.h
@@ -28,9 +28,19 @@
 
 - (void)initializeWithInitialWorlds:(NSArray *)oInitialWorlds;
 
-- (Score*) getScoreWithScoreId:(NSString*)scoreId;
+- (Score*)getScoreWithScoreId:(NSString*)scoreId;
 
-- (World*) getWorldWithWorldId:(NSString*)worldId;
+- (World*)getWorldWithWorldId:(NSString*)worldId;
+
+- (int)getLevelCount;
+
+- (int)getLevelCountInWorld:(World *)world;
+
+- (int)getWorldCount:(BOOL)withLevels;
+
+- (int)getCompletedLevelCount;
+
+- (int)getCompletedWorldCount;
 
 + (LevelUp*)getInstance;
 
